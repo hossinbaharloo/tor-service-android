930c17eb619eeb0a387037e81b9cb671baa36305
Hossein Baharloo <54281095+hossin98@users.noreply.github.com>
2020-12-28 20:22:00 +0330

    Add files via upload

---
+++ tor.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#builder => hossien baharlou
+echo 'Welcome'
+pkg install proxychains-ng
+
+echo 'obfs4 85.143.214.143:6697 2A92890043632E0C126029BED54FD68C8B224507 cert=EtuUbek0ufQp3ggoGPya28Lcr6duKwOexVcqT4MopHWikUnhsyVFTG1cMzHTYVKipZDfKw iat-mode=0' >> $PREFIX/etc/tor/torrc
+echo 'obfs4 134.209.88.233:29930 782D9BFAEF45577A24BE70C77DEA4185F51752E5 cert=Gf09UaEwYER0T1xey1xX0SUWpTS8+819ssikzCQq3TSmY+dqMfAtNyYda7ifzAV/8Fa1XA iat-mode=0' >> $PREFIX/etc/tor/torrc
+echo 'obfs4 85.143.214.143:6697 2A92890043632E0C126029BED54FD68C8B224507 cert=EtuUbek0ufQp3ggoGPya28Lcr6duKwOexVcqT4MopHWikUnhsyVFTG1cMzHTYVKipZDfKw iat-mode=0' >> $PREFIX/etc/tor/torrc
+echo 'obfs4 134.209.88.233:29930 782D9BFAEF45577A24BE70C77DEA4185F51752E5 cert=Gf09UaEwYER0T1xey1xX0SUWpTS8+819ssikzCQq3TSmY+dqMfAtNyYda7ifzAV/8Fa1XA iat-mode=0' >> $PREFIX/etc/tor/torrc
+echo 'obfs4 85.143.214.143:6697 2A92890043632E0C126029BED54FD68C8B224507 cert=EtuUbek0ufQp3ggoGPya28Lcr6duKwOexVcqT4MopHWikUnhsyVFTG1cMzHTYVKipZDfKw iat-mode=0' >> $PREFIX/etc/tor/torrc
+echo 'obfs4 134.209.88.233:29930 782D9BFAEF45577A24BE70C77DEA4185F51752E5 cert=Gf09UaEwYER0T1xey1xX0SUWpTS8+819ssikzCQq3TSmY+dqMfAtNyYda7ifzAV/8Fa1XA iat-mode=0' >> $PREFIX/etc/tor/torrc
+
+
